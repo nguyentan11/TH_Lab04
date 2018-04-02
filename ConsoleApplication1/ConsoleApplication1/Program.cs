561e4d97de8bfc34364694144fb831f8e84eccba
nguyentan11 <35859403+nguyentan11@users.noreply.github.com>
2018-04-02 22:38:23 +0700

    Bai tap 2 tuan 4 ( ket noi TCP )

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipserver = new IPEndPoint(IPAddress.Parse("192.168.1.108"), 80);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.SendTimeout = 1000;
+            client.ReceiveTimeout = 1000;
+            try
+            {
+                client.Connect(ipserver);
+                Console.WriteLine(ipserver.ToString() + " is opened.");
+                client.Close();
+            }
+            catch 
+            {
+                Console.WriteLine(ipserver.ToString() + " is closed.");
+            }
+        }
+    }
+}
